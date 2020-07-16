9c387dd1d10b64fef0764dc6b542c65bc01c7bf0
joseph Evangelista <oootep123@gmail.com>
2020-07-15 23:30:21 -0400

    initial implementation of modal of tour cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
+// tour section
+const tourDetailsBtn = document.querySelector(".tour-details")
+const modal = document.querySelector(".tour-modal")
 
+tourDetailsBtn.addEventListener("click", function () {
+  modal.style.display = "block"
+})
